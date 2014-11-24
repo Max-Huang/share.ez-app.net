9482dd625d0456eee2f3dc4a9957399320f93445
Max Huang <max@ez-app.net>
2014-11-25 00:34:05 +0800

    0

--- share.ez-app.net/index.php
+++ share.ez-app.net/index.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * share.ez-app.net
- * Signed-off-by: Max Huang <max@ez-app.net>
- *
- * ===
- */
+$t = '分享程式碼';
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,7 +9,7 @@
 	content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
 </head>
 <body>
-<h2>share.ez-app.net 分享程式碼</h2>
+<h2>share.ez-app.net <?= $t; ?></h2>
 <hr>
 Max Huang<br>
 <a href="mailto:max@ez-app.net">e-mail me</a>.
