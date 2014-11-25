eec8a37906aa5cab481c4f45630bceb3a35db691
Max Huang <max@ez-app.net>
2014-11-25 22:07:27 +0800

    0

--- share.ez-app.net/index.php
+++ share.ez-app.net/index.php
@@ -10,7 +10,7 @@ $t = '分享程式碼';
 	content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
 </head>
 <body>
-<h2>share.ez-app.net <?= $t; ?></h2>
+<h2>share.ez-app.net <?= $t ?></h2>
 <hr>
 Max Huang<br>
 <a href="mailto:max@ez-app.net">e-mail me</a>.
